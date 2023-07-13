07fd5946488032a04c1006ac03e4adb24e5dee28
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-07-13 15:50:14 +0600

    update

---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {
+        count: 0,
+    },
+    reducers: {
+        increment: state => {
+            state.count = state.count + 1;
+        },
+        decrement: state => {
+            state.count = state.count - 1;
+        },
+        reset: state => {
+            state.count = 0
+        },
+        countByAmount: state => {
+            state.count = state.count + 4
+        }
+    }
+});
